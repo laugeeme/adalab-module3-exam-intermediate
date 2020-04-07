15e0565bd9a072fa5366367ba7f17311d4b60764
laurageeme <registro.laura@gmail.com>
2020-04-07 08:29:31 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-useless-constructor */
+import React from 'react';
+import '../stylesheet/App.css';
+import '../data/pokemon';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+
+      </div>
+    );
+  }
+}
+
+export default App;
