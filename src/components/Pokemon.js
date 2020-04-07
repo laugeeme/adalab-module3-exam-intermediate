e8765f0bda08ce97ce647f8c7028a4ed6badeb07
laurageeme <registro.laura@gmail.com>
2020-04-07 10:02:34 +0200

    final styles

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,29 +1,28 @@
 import React from 'react';
 
-
 function Pokemon(props) {
-    console.log(props)
-    return (
-
+  console.log(props);
+  return (
+    <div className="pokemonCard">
       <li className="pokemon" id={props.pokeId}>
         <div className="pokemon-image">
-            <img src={props.pokeUrl} alt={props.pokeName}></img>
+          <img src={props.pokeUrl} alt={props.pokeName}></img>
         </div>
 
         <div className="pokemon-name">
-            <h2>{props.pokeName}</h2>
+          <h2>{props.pokeName}</h2>
         </div>
 
         <div className="pokemon-types">
-             <ul className="type">
-                {props.pokeTypes.map(pokeType =>
-                    <li>{pokeType}</li>) }
-            </ul> 
+          <ul className="type">
+            {props.pokeTypes.map((pokeType) => (
+              <li>{pokeType}</li>
+            ))}
+          </ul>
         </div>
-  
       </li>
-    );
-  }
+    </div>
+  );
+}
 
-
-  export default Pokemon; 
+export default Pokemon;
