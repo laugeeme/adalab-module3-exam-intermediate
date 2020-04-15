c26bfd30d76d6cf09c681c21fa348f2f149dadbb
laurageeme <registro.laura@gmail.com>
2020-04-15 20:49:33 +0200

    run docs

---
+++ docs/build/precache-manifest.5a1b0a633498eea25f9f4757dc9ec6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd14a446180c541222aa6520de56834",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55699decc736bedad623",
+    "url": "./static/css/main.988a4767.chunk.css"
+  },
+  {
+    "revision": "c8e58728814fba3d2b6e",
+    "url": "./static/js/2.4c2a02b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4c2a02b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55699decc736bedad623",
+    "url": "./static/js/main.9261d353.chunk.js"
+  },
+  {
+    "revision": "b226d842449de8d3973e",
+    "url": "./static/js/runtime-main.f9ecd7be.js"
+  }
+]);
